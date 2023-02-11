4df75ac6cec46fe38bb9d9da7648674950030dc0
Jeremiah Chesley <msgrmiah@gmail.com>
2023-02-11 16:28:35 -0700

    start user route push chooment route

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { User, Blog } = require('../../models');
+
+// create a user:
+
+//sign-in
+
+//validation
+
+//Password
+
+//logout destroy session
