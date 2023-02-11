4add44a989bbe473bddff6ad619709fad6e22d14
Jeremiah Chesley <msgrmiah@gmail.com>
2023-02-11 16:38:41 -0700

    fixed user routes and added comment route

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,5 +2,30 @@ const router = require('express').Router();
 const { Blog, Comment, User } = require('../../models');
 
 //return all comments
+router.get('/', async (req, res) => {
+  try {
+    const commentData = await Comment.findAll({
+      include: [User, Blog],
+    });
+
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //create a comment
+router.post('/', async (req, res) => {
+  try {
+    const commentData = await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
